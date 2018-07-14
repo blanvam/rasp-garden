7f016b24115e4a2f1b287165c419e57fb8d1659f
Victor M. Macias Ariza <blanvam@gmail.com>
2018-07-14 14:34:33 +0200

    Updated client definition

--- topic/client.go
+++ topic/client.go
@@ -5,8 +5,8 @@ type CallbackHandler func(topic string, clientID string, config []byte)
 // Client interface definition for a mqtt conexion
 type Client interface {
 	IsConnected(c chan bool)
-	Disconnect(c chan error, quiesce uint)
 	Connect(c chan error)
+	Disconnect(c chan error, quiesce uint)
 	Publish(c chan error, topic string, qos uint8, payload interface{})
 	Subscribe(c chan error, topic string, qos uint8, callback CallbackHandler)
 	Unsubscribe(c chan error, topic string)
